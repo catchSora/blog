2ef5a5bb4a1beb96c70945569648915ba0420acf
catchSora <2292223774@qq.com>
2018-10-30 19:59:15 +0800

    sql file

---
+++ sql/blog.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : blog
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2018-10-30 13:58:28
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for blog_user
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_user`;
+CREATE TABLE `blog_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(36) DEFAULT NULL COMMENT '名称',
+  `login_name` varchar(64) DEFAULT NULL COMMENT '登录用户名',
+  `login_password` varchar(32) DEFAULT NULL COMMENT '登录密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of blog_user
+-- ----------------------------
+INSERT INTO `blog_user` VALUES ('1', 'one', '145', '123456');
